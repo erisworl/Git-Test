ef3b3463d9c6c2bbcd779065fa86a1fadffce6b3
Erik Worley <erisworl@ut.utm.edu>
2019-01-17 05:07:17 -0600

    Updated code, random number gen.

--- GitTester/Program.cs
+++ GitTester/Program.cs
@@ -8,7 +8,30 @@ namespace GitTester
     {
         static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            int userNumber;
+            int counter = 0;
+            int number = new Random().Next(1, 101);
+            
+            Console.WriteLine("I am thinking of a number between 1-100; what is it?");
+            userNumber = Convert.ToInt32(Console.ReadLine());
+            counter++;
+            while (userNumber != number)
+            {
+                if (userNumber < number)
+                {
+                    Console.WriteLine("The number I am thinking of is higher, try again!");
+                    counter++;
+                    userNumber = Convert.ToInt32(Console.ReadLine());
+                }
+                else
+                {
+                    Console.WriteLine("The number I am thinking of is lower, try again!");
+                    counter++;
+                    userNumber = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+
+            Console.WriteLine("You guessed the right number in " + counter + " guess(es)!");
 
             Console.ReadKey();
         }
